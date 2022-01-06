027bb8775ab16a0f04fff25a972561b57fd245c4
NguyenThoDiep <59200596+NguyenThoDiep@users.noreply.github.com>
2022-01-06 22:30:01 +0700

    fix
    fix

--- client/src/components/views/Dashboard/Dashboard.js
+++ client/src/components/views/Dashboard/Dashboard.js
@@ -2,16 +2,20 @@ import React, { useEffect } from 'react';
 import logo from '../resources/logo.png';
 import ActiveUsersList from './components/ActiveUsersList/ActiveUsersList';
 import * as webRTCHandler from '../../../utils/webRTC/webRTCHandler';
+import * as webRTCGroupHandler from '../../../utils/webRTC/webRTCGroupCallHandler';
 import DirectCall from './components/DirectCall/DirectCall';
 import { connect } from 'react-redux';
 import DashboardInformation from './components/DashboardInformation/DashboardInformation';
 import { callStates } from '../../../store/_actions/callActions';
+import GroupCallRoomsList from './components/GroupCallRoomsList/GroupCallRoomsList';
+import GroupCall from './components/GroupCall/GroupCall';
 
 import './Dashboard.css';
 
 const Dashboard = ({ username, callState }) => {
   useEffect(() => {
     webRTCHandler.getLocalStream();
+    webRTCGroupHandler.connectWithMyPeer();
   }, []);
 
   return (
@@ -19,11 +23,12 @@ const Dashboard = ({ username, callState }) => {
       <div className='dashboard_left_section'>
         <div className='dashboard_content_container'>
           <DirectCall />
+          <GroupCall />
           {callState !== callStates.CALL_IN_PROGRESS && <DashboardInformation username={username} />}
         </div>
-        {/* <div className='dashboard_rooms_container background_secondary_color'>
-            rooms
-        </div> */}
+        <div className='dashboard_rooms_container background_secondary_color'>
+            <GroupCallRoomsList />
+        </div>
       </div>
       <div className='dashboard_right_section background_secondary_color'>
         <div className='dashboard_active_users_list'>
